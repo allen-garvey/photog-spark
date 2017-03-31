18d18f424b35b2c8df1ba44fdf6cf791865db9f5
Allen Garvey <allen.garvey@gmail.com>
2017-03-31 14:41:55 -0400

    Add more characters to be escaped by uriencode

--- src/main/kotlin/views/BaseView.kt
+++ src/main/kotlin/views/BaseView.kt
@@ -4,7 +4,27 @@ package views
  * Created by allen on 3/31/17.
  */
 abstract class BaseView {
+    //based on: https://en.wikipedia.org/wiki/Percent-encoding
     fun uriEncode(s: String): String{
-        return s.replace("%", "%25").replace(" ", "%20")
+        return s
+                .replace("%", "%25")
+                .replace(" ", "%20")
+                .replace("!", "%21")
+                .replace("#", "%23")
+                .replace("$", "%24")
+                .replace("&", "%26")
+                .replace("'", "%27")
+                .replace("(", "%28")
+                .replace(")", "%29")
+                .replace("*", "%2A")
+                .replace("+", "%2B")
+                .replace(",", "%2C")
+                .replace(":", "%3A")
+                .replace(";", "%3B")
+                .replace("=", "%3D")
+                .replace("?", "%3F")
+                .replace("@", "%40")
+                .replace("[", "%5B")
+                .replace("]", "%5D")
     }
 }
