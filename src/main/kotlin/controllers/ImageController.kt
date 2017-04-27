f477bb71b25e9882b45e7371ab8595d06e7d581b
Allen Garvey <allen.garvey@gmail.com>
2017-04-27 19:03:48 -0400

    Add next and previous image links to album image show pages

--- src/main/kotlin/controllers/ImageController.kt
+++ src/main/kotlin/controllers/ImageController.kt
@@ -20,7 +20,16 @@ object ImageController{
     fun showAlbumImage(request: Request, response: Response, albumIdParameterName: String, imageIdParameterName: String): ModelAndView {
         val image: Image = SqliteController.selectImage(request.params(imageIdParameterName)) ?:  return ErrorController.notFound(request, response)
         val album: Album = SqliteController.selectAlbum(request.params(albumIdParameterName)) ?: return ErrorController.notFound(request, response)
+        val images: MutableList<Image> = SqliteController.imagesForAlbum(request.params(albumIdParameterName))
 
-        return ModelAndView(hashMapOf(Pair("image", image), Pair("parent_album", album), Pair("albums", SqliteController.albumsForImage(request.params(imageIdParameterName)))), "image_show.hbs")
+        val imageIndex: Int = images.indexOf(image)
+        //make sure this image is part of the parent album
+        if(imageIndex < 0){
+            return ErrorController.notFound(request, response)
+        }
+        val previousImage: Image? = if(imageIndex > 0) images.get(imageIndex - 1) else null
+        val nextImage: Image? = if(imageIndex < images.size - 1) images.get(imageIndex + 1) else null
+
+        return ModelAndView(hashMapOf(Pair("image", image), Pair("parent_album", album), Pair("previous_image", previousImage), Pair("next_image", nextImage), Pair("albums", SqliteController.albumsForImage(request.params(imageIdParameterName)))), "image_show.hbs")
     }
 }
