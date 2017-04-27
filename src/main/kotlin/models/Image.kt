d63e7208fbc7983d58946f539f81e6372c5d5479
Allen Garvey <allen.garvey@gmail.com>
2017-04-27 19:03:13 -0400

    Add custom equals method to image data class

--- src/main/kotlin/models/Image.kt
+++ src/main/kotlin/models/Image.kt
@@ -4,4 +4,8 @@ package models
  * Created by allen on 3/29/17.
  */
 
-data class Image(val id: String, val versionId: String, val path: String, var thumbnail: Thumbnail?)
+data class Image(val id: String, val versionId: String, val path: String, var thumbnail: Thumbnail?){
+    fun equals(otherImage: Image): Boolean {
+        return otherImage.id == this.id
+    }
+}
