7939fd7f6fe38372af6b98499895eb9953428fc3
Allen Garvey <allen.garvey@gmail.com>
2017-05-21 15:44:22 -0400

    Create shared template for list of links

--- src/main/kotlin/templates/SharedTemplate.kt
+++ src/main/kotlin/templates/SharedTemplate.kt
@@ -8,6 +8,7 @@ import kotlinx.html.*
 import models.Folder
 import models.Image
 import views.ImageView
+import views.Link
 
 object SharedTemplate{
     fun imageListPage(pageTitle: String, images: MutableList<Image>, imageUrlFunc: (Image) -> String): String{
@@ -21,6 +22,21 @@ object SharedTemplate{
                         }
                         if(it.isFavorite){
                             div("heart")
+                        }
+                    }
+                }
+            }
+        }, subTitle = pageTitle)
+    }
+
+    fun textListPage(listTitle: String, links: List<Link>, pageTitle: String): String{
+        return Layout.mainLayout({
+            h2{ +listTitle }
+            ul("text-list"){
+                links.forEach {
+                    li {
+                        a(it.url){
+                            +it.title
                         }
                     }
                 }
---
+++ src/main/kotlin/views/Link.kt
@@ -0,0 +1,6 @@
+package views
+
+/**
+ * Created by allen on 5/21/17.
+ */
+data class Link(val title: String, val url: String)
