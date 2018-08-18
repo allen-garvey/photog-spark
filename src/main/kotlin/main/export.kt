063ab145f83467002cb26ef35fc9f0560e934398
Allen Garvey <allen.garvey@gmail.com>
2018-08-18 11:02:16 -0400

    Edit export script to use subqueries to select ids for related fields

--- src/main/kotlin/main/export.kt
+++ src/main/kotlin/main/export.kt
@@ -3,6 +3,11 @@ package main
 import controllers.SqliteController
 import java.sql.Timestamp
 
+val IMAGES_TABLE_NAME = "images"
+val ALBUMS_TABLE_NAME = "albums"
+val PEOPLE_TABLE_NAME = "persons"
+val FOLDERS_TABLE_NAME = "folders"
+
 
 fun sqlEscapeOptionalString(s: String?): String{
     if(s == null){
@@ -42,6 +47,22 @@ fun sqlTimestamp(t: Timestamp): String{
     return "to_timestamp('${t.toString().replace(Regex("\\.0$"), "")}', 'YYYY-MM-DD HH:MI:SS')"
 }
 
+fun relatedImageId(imageId: String): String{
+    return "(SELECT ${IMAGES_TABLE_NAME}.id FROM ${IMAGES_TABLE_NAME} WHERE ${IMAGES_TABLE_NAME}.apple_photos_id = ${imageId} LIMIT 1)"
+}
+
+fun relatedPersonId(personId: String): String{
+    return "(SELECT ${PEOPLE_TABLE_NAME}.id FROM ${PEOPLE_TABLE_NAME} WHERE ${PEOPLE_TABLE_NAME}.apple_photos_id = ${personId} LIMIT 1)"
+}
+
+fun relatedAlbumId(albumId: String): String{
+    return "(SELECT ${ALBUMS_TABLE_NAME}.id FROM ${ALBUMS_TABLE_NAME} WHERE ${ALBUMS_TABLE_NAME}.apple_photos_id = ${albumId} LIMIT 1)"
+}
+
+fun relatedFolderUuid(folderUuid: String): String{
+    return "(SELECT ${FOLDERS_TABLE_NAME}.id FROM ${FOLDERS_TABLE_NAME} WHERE ${FOLDERS_TABLE_NAME}.apple_photos_uuid = ${folderUuid} LIMIT 1)"
+}
+
 
 fun main(args: Array<String>) {
     if(args.size >= 1){
@@ -51,12 +72,12 @@ fun main(args: Array<String>) {
 
     println("\n\n--Folders\n")
     SqliteController.selectAllFolders().forEach{
-        println("INSERT INTO folders (apple_photos_uuid, name) VALUES (${sqlEscapeString(it.uuid)}, ${sqlEscapeString(it.name)});")
+        println("INSERT INTO ${FOLDERS_TABLE_NAME} (apple_photos_uuid, name) VALUES (${sqlEscapeString(it.uuid)}, ${sqlEscapeString(it.name)});")
     }
 
     println("\n\n--Images\n")
     SqliteController.selectAllImages().forEach{
-        println("INSERT INTO images (apple_photos_id, creation_time, master_path, thumbnail_path, mini_thumbnail_path, is_favorite) VALUES (${it.id}, ${sqlTimestamp(it.creation!!)}, ${sqlEscapeString(it.path)}, ${sqlEscapeString(it.thumbnail!!.thumbnailPath)}, ${sqlEscapeString(it.thumbnail!!.miniThumbnailPath)}, ${sqlBool(it.isFavorite)});")
+        println("INSERT INTO ${IMAGES_TABLE_NAME} (apple_photos_id, creation_time, master_path, thumbnail_path, mini_thumbnail_path, is_favorite) VALUES (${it.id}, ${sqlTimestamp(it.creation!!)}, ${sqlEscapeString(it.path)}, ${sqlEscapeString(it.thumbnail!!.thumbnailPath)}, ${sqlEscapeString(it.thumbnail!!.miniThumbnailPath)}, ${sqlBool(it.isFavorite)});")
     }
 
     
@@ -64,7 +85,7 @@ fun main(args: Array<String>) {
     var albums = SqliteController.selectAllAlbums()
     albums.sortBy({it.id.toInt()})
     albums.forEach{
-        println("INSERT INTO albums (apple_photos_id, name, folder_id, folder_order, cover_image_id) VALUES (${it.id}, ${sqlEscapeString(it.name)}, ${sqlEscapeString(it.folderUuid)}, ${it.folderOrder!!}, ${sqlOptionalInt(it.coverImage?.id)});")
+        println("INSERT INTO ${ALBUMS_TABLE_NAME} (apple_photos_id, name, folder_id, folder_order, cover_image_id) VALUES (${it.id}, ${sqlEscapeString(it.name)}, ${relatedFolderUuid(sqlEscapeString(it.folderUuid))}, ${it.folderOrder!!}, ${relatedImageId(it.coverImage!!.id)});")
     }
 
 
@@ -72,19 +93,19 @@ fun main(args: Array<String>) {
     var people = SqliteController.selectAllPeople()
     people.sortBy({it.id.toInt()})
     people.forEach{
-        println("INSERT INTO persons (apple_photos_id, name, cover_image_id) VALUES (${it.id}, ${sqlEscapeString(it.name)}, ${it.coverImageId!!});")
+        println("INSERT INTO ${PEOPLE_TABLE_NAME} (apple_photos_id, name, cover_image_id) VALUES (${it.id}, ${sqlEscapeString(it.name)}, ${relatedImageId(it.coverImageId!!)});")
     }
 
 
 
     println("\n\n--Person Images\n")
     SqliteController.selectAllPersonImages().forEach{
-        println("INSERT INTO person_images (person_id, image_id) VALUES (${it.personId}, ${it.imageId});")
+        println("INSERT INTO person_images (person_id, image_id) VALUES (${relatedPersonId(it.personId)}, ${relatedImageId(it.imageId)});")
     }
 
     println("\n\n--Album Images\n")
     SqliteController.selectAllAlbumImages().forEach{
-        println("INSERT INTO album_images (album_id, image_id, order) VALUES (${it.albumId}, ${it.imageId}, ${it.order});")
+        println("INSERT INTO album_images (album_id, image_id, order) VALUES (${relatedAlbumId(it.albumId)}, ${relatedImageId(it.imageId)}, ${it.order});")
     }
 
 }
