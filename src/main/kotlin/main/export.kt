5235392247fb7df31932aad23c20126e8152ecb3
Allen Garvey <allen.garvey@gmail.com>
2019-02-24 10:04:27 -0500

    Fix missing semicolon in sql statement when inserting import

--- src/main/kotlin/main/export.kt
+++ src/main/kotlin/main/export.kt
@@ -83,7 +83,7 @@ fun main(args: Array<String>) {
 
     //create import resources
     println("\n\n--Imports\n")
-    println("INSERT INTO imports (id, import_time ${TIMESTAMPS_COLUMN_NAMES} ) VALUES (${IMPORT_ID}, now() ${TIMESTAMPS_COLUMN_VALUES})")
+    println("INSERT INTO imports (id, import_time ${TIMESTAMPS_COLUMN_NAMES} ) VALUES (${IMPORT_ID}, now() ${TIMESTAMPS_COLUMN_VALUES});")
 
 
     println("\n\n--Tags (called Folders in Apple Photos)\n")
